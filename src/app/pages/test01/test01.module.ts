33f884f2c30a39176b24871a3cbc0b2c36a733e8
Vinycius Oliveira <vinycius0550@gmail.com>
2023-12-08 14:46:00 -0300

    ajustes diversos

--- src/app/pages/test01/test01.module.ts
+++ src/app/pages/test01/test01.module.ts
@@ -6,8 +6,7 @@ import { Test01RoutingModule } from './test01-routing.module';
 import { Test01Component } from './test01.component';
 
 @NgModule({
-    imports: [CommonModule, Test01RoutingModule, ],
-    exports: [Test01Component],
+    imports: [CommonModule, Test01RoutingModule],
     declarations: [Test01Component],
 })
 export class Test01Module {}
--- src/app/pages/test02/test02.module.ts
+++ src/app/pages/test02/test02.module.ts
@@ -6,8 +6,7 @@ import { Test02RoutingModule } from './test02-routing.module';
 import { Test02Component } from './test02.component';
 
 @NgModule({
-    imports: [CommonModule, Test02RoutingModule, ],
-    exports: [Test02Component],
+    imports: [CommonModule, Test02RoutingModule],
     declarations: [Test02Component],
 })
 export class Test02Module {}
